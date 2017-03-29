27a871da2fa072ff9d946067c118bb8add3e0d9b
innovaFTA <aalmis@innova.com.tr>
2017-03-29 11:00:13 +0300

    innovagrid module

---
+++ src/innovagrid/innova-grid-paging.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input, OnInit, DoCheck, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+    selector: 'innova-grid-paging',
+    template: `
+       <nav>
+                    <ul class="pagination">
+                           <li style="cursor:pointer" [class.disabled]="currentPage==1"
+                         class="page-item">
+                         <a class="page-link" (click)="onChange(1)">İlk</a>
+                        </li>
+                        <li style="cursor:pointer" [class.disabled]="currentPage==1" class="page-item">
+                        <a class="page-link" (click)="onChange(currentPage-1)">Önceki</a>
+                        </li>
+                        <li style="cursor:pointer" *ngFor="let page of pages" [class.active]="currentPage==page"
+                         class="page-item"><a class="page-link" (click)="onChange(page)">{{page}}</a>
+                        </li>
+                        <li style="cursor:pointer" [class.disabled]="currentPage==pages.length"
+                         class="page-item">
+                         <a class="page-link" (click)="onChange(currentPage+1)">Sonraki</a>
+                        </li>
+                           <li style="cursor:pointer" [class.disabled]="currentPage==pages.length"
+                         class="page-item">
+                         <a class="page-link" (click)="onChange(pages.length)">Son</a>
+                        </li>
+                    </ul>
+                </nav>
+    `
+})
+
+export class InnovaGridPagingComponent implements OnInit {
+    @Input() currentPage: number;
+    @Input() pageCount: number;
+
+    @Output() onChangePage = new EventEmitter();
+
+    pages: number[];
+
+    constructor() { }
+
+    private createPages() {
+        this.pages = [];
+        for (var i = 1; i <= this.pageCount; i++) {
+            this.pages.push(i);
+        }
+    }
+
+    onChange(page) {
+        this.onChangePage.emit({ newPageIndex: page });
+    }
+
+
+    ngOnInit() {
+        this.createPages();
+    }
+
+
+
+}
---
+++ src/innovagrid/innova-grid.options.ts
@@ -0,0 +1,85 @@
+import { Observable } from 'rxjs/Observable';
+import { PipeTransform } from '@angular/core';
+
+
+export class InnovaGridOptions {
+    // public fields
+    public DataSource: Observable<any>;
+    public isLoading = false;
+    public PrimaryFieldName: string;
+    public Columns: InnovaGridColumn[];
+    public CurrentDataLength: number;
+    //private Fields
+    private Actions: InnovaGridActions[];
+    private CondionalRowClasses?: ConditionalClass[];
+    private Name = 'Default';
+    private selectedPage: number;
+
+    public Initialize(page?: number) {
+        if (page) {
+            this.selectedPage = page;
+        } else {
+            this.selectedPage = 1;
+        }
+    }
+
+    constructor(_name: string, _cols: InnovaGridColumn[], _actions: InnovaGridActions[], _primaryFieldName: string,
+        _condionalRowClasses?: ConditionalClass[]) {
+        this.Name = _name;
+        this.Columns = _cols;
+        this.Actions = _actions;
+        this.PrimaryFieldName = _primaryFieldName;
+        this.CondionalRowClasses = _condionalRowClasses;
+    }
+}
+
+export interface InnovaGridColumn {
+    Header: string;
+    DataField?: string;
+    Width?: number;
+    PipeElements?: PipeElement[];
+}
+
+export interface PipeElement {
+    Pipe: PipeTransform;
+    PipeParameters?: any[];
+}
+
+export interface ConditionalClass {
+    Condition: Condition;
+    Class: string;
+}
+
+export interface InnovaGridActions {
+    ActionElementType: ActionElType;
+    text?: string;
+    DemandedDataFields?: string;
+    CommandArg: string;
+    imgSrc?: string;
+    Class?: string;
+    IconClass?: string;
+    title?: string;
+    ActionVisibilityConditions?: Condition[];
+    constWidth?: string;
+}
+
+export enum ActionElType {
+    button,
+    img,
+    link,
+    icon
+}
+
+export interface Condition {
+    DataField: string;
+    ConditionValue: any;
+    ConditionType: ConditionType;
+}
+
+export enum ConditionType {
+    Equal = 0,
+    GraterThen = 1,
+    GraterThenEqual = 2,
+    SMallerThen = 3,
+    SMallerThenEqual = 4
+}
