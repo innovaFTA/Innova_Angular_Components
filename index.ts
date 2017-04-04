f7ebd65fba0087a560ef1bce5c2f99ce85c3f574
innovaFTA <aalmis@innova.com.tr>
2017-04-04 11:07:05 +0300

    index.ts birleşti.

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-export * from './src/innovagrid/innova-grid.module';
-export * from './src/innovagrid/innova-grid.options';
-
-//busy module
-export * from './innova-busy-directive';
-export * from './innova-busy.service';
-export * from './innova-busy-config';
-export * from './innova-busy.module';
+ export * from './src/innovagrid/innova-grid.module';
+ export * from './src/innovagrid/innova-grid.options';
+ 
+ //innova busy
+ export * from './src/innovabusy/innova-busy-directive';
+ export * from './src/innovabusy/innova-busy.service';
+ export * from './src/innovabusy/innova-busy-config';
+ export * from './src/innovabusy/innova-busy.module'; 
